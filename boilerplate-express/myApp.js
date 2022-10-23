a046dc929204ba391105837ce017d23734f4b500
hannah-eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2022-10-23 23:52:14 +1000

    first part express challenge

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,39 +1,26 @@
-let express = require('express');
+require("dotenv").config();
+
+let express = require("express");
 let app = express();
 
+console.log("Hello World");
 
+// Normal usage
+// app.use(express.static(__dirname + "/public"));
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/", function (req, res) {
+  // res.send("Hello Express");
+  const absolutePath = __dirname + "/views/index.html";
+  res.sendFile(absolutePath);
+});
 
+app.get("/json", function (req, res) {
+  const message =
+    process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json";
 
+  res.json({ message });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
